8cf5d5d6192743d9ec02aa9f28f588a4dd9e9518
lykj <766822325@qq.com>
2020-09-23 14:15:43 +0800

    初始化

---
+++ skinme/App.vue
@@ -0,0 +1,29 @@
+<script>
+	export default {
+		onLaunch: function() {
+			window.document.documentElement.setAttribute('data-theme', 'theme1');
+			// document.getElementsByTagName('uni-page-wrapper').offsetParent.clientHeight=this.windowHeight+'px'
+			console.log(document.getElementsByTagName('uni-page-wrapper'))
+		},
+		onShow: function() {
+			console.log('App Show')
+		},
+		onHide: function() {
+			console.log('App Hide')
+		}
+	}
+</script>
+
+<style>
+	/*每个页面公共css */ 
+</style>
+<style lang="scss">
+	// page{
+	// 	@include bg_img(theme)
+	// }
+	
+	// @include 指令可以将混入（mixin）引入到文档中。
+	uni-page-wrapper{
+		@include bg_img(theme)
+	}
+</style>
---
+++ skinme/pages/index/index.vue
@@ -0,0 +1,67 @@
+<template>
+	<view class="content">
+		<image class="logo"></image>
+		<view class="text-area">
+			<text class="title">{{title}}</text>
+		</view>
+		<view class="">
+			
+		</view>
+		<view>
+			<button @tap="doSkinme('theme1')">皮肤1</button>
+			<button @tap="doSkinme('theme2')">皮肤2</button>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello',
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+			doSkinme(theme){
+				window.document.documentElement.setAttribute('data-theme', theme);
+			}
+		}
+	}
+</script>
+
+<style>
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+	.logo {
+		height: 200rpx;
+		width: 200rpx;
+		margin-top: 200rpx;
+		margin-left: auto;
+		margin-right: auto;
+		margin-bottom: 50rpx;
+	}
+	.text-area {
+		display: flex;
+		justify-content: center;
+	}
+
+	.title {
+		font-size: 36rpx;
+	}
+</style>
+<style lang="scss">
+	.logo {
+		@include login_content(theme)
+	}
+	.title {
+		@include bg_color(theme)
+		@include ft_color(theme)
+	}
+</style>
